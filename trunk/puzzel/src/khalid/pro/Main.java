04446d3a97c8ad9447df0af97d26c84d7856440a
khalidpro <khalidpro@c17fd579-364d-01aa-092e-8ab77f1fc4f9>
2013-01-28 15:38:35 +0000

    l'ajout de fenetre detail

--- trunk/puzzel/src/khalid/pro/Main.java
+++ trunk/puzzel/src/khalid/pro/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String name = System.getProperty ( "os.name" );
-		System.out.println(name);
+	//	String name = System.getProperty ( "os.name" );
+	//	System.out.println(name);
 		Fenetre f=new Fenetre();
+		
 		 
 
 	}
